f6e645a64deb5642888239e75f7c6e42f3a9d300
EvilPoseidon3 <160077853+EvilPoseidon3@users.noreply.github.com>
2024-05-15 18:14:50 -0500

    first commit after I forgot to start on github

---
+++ rubycipher.rb
@@ -0,0 +1,37 @@
+def is_lower?(c)
+  c != c.upcase
+end
+
+def is_upper?(c)
+  c != c.downcase
+end
+
+def caesar_cipher(string, number)
+  alphabet = [["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+  ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]]
+  
+  cryptic_string =[]
+  string.each_char {|char|
+    if is_lower?(char)
+      lower_index = alphabet[0].index("#{char.to_s}") + number
+      if lower_index > 25
+        lower_index = lower_index - 26
+      end
+      cryptic_string.push(alphabet[0][lower_index]) 
+    elsif is_upper?(char)
+      upper_index = alphabet[1].index("#{char.to_s}") + number
+      if upper_index > 25
+        upper_index = upper_index - 26
+      end
+      cryptic_string.push(alphabet[1][upper_index])
+      puts -(alphabet[1].index("#{char.to_s}"))
+    else
+      cryptic_string.push(char) 
+    end  
+      }
+  return cryptic_string.join
+end
+
+puts caesar_cipher("What a string!", 5)
+puts "what a string".to_i
+
